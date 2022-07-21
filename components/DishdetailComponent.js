f288afc16e2cafe95ace7814ecaccd4aaecc714c
Hoàng Nhã Thy <77708167+thynhacute@users.noreply.github.com>
2022-07-21 13:58:28 +0700

    React Animation Components

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,17 +1,34 @@
-function RenderComments({comments, postComment, dishId}) {
-      
-    . . .
-    
-                        <CommentForm dishId={dishId} postComment={postComment} />
-                        
-    . . .
-    
-                this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
-                
-    . . .
-    
-                                postComment={props.postComment}
-    
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
-    
+. . .
+
+            <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+            <Card>
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+            </FadeTransform>
+            
+. . .
+
+                    <Stagger in>
+                        {comments.map((comment) => {
+                            return (
+                                <Fade in>
+                                <li key={comment.id}>
+                                <p>{comment.comment}</p>
+                                <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                                </li>
+                                </Fade>
+                            );
+                        })}
+                        </Stagger>
+                        
 
--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,34 +1,19 @@
-import { Loading } from './LoadingComponent';
+import { FadeTransform } from 'react-animation-components';
 
 . . .
 
-function RenderCard({item, isLoading, errMess}) {
-    
-    if (isLoading) {
-        return(
-                <Loading />
-        );
-    }
-    else if (errMess) {
-        return(
-                <h4>{errMess}</h4>
-        );
-    }
-    else 
-        return(
-            <Card>
-                <CardImg src={item.image} alt={item.name} />
-                <CardBody>
-                <CardTitle>{item.name}</CardTitle>
-                {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
-                <CardText>{item.description}</CardText>
-                </CardBody>
-            </Card>
-        );
-
-}
-
-. . .
-
-                    <RenderCard item={props.dish} isLoading={props.dishesLoading} errMess={props.dishesErrMess}  />
+            <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+                <Card>
+                    <CardImg src={baseUrl + item.image} alt={item.name} />
+                    <CardBody>
+                    <CardTitle>{item.name}</CardTitle>
+                    {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
+                    <CardText>{item.description}</CardText>
+                    </CardBody>
+                </Card>
+            </FadeTransform>
 
