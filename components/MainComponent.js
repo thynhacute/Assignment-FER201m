40c27abc81d167540cdb5a7db52cd43f0e9ac93b
Hoàng Nhã Thy <77708167+thynhacute@users.noreply.github.com>
2022-07-21 13:56:36 +0700

    React Animations

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,12 +1,20 @@
-import { postComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 . . .
 
-  postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment))
-
-. . .
-
-            postComment={this.props.postComment}
+          <TransitionGroup>
+            <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+              <Switch location={this.props.location}>
+                  <Route path='/home' component={HomePage} />
+                  <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />} />
+                  <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
+                  <Route path='/menu/:dishId' component={DishWithId} />
+                  <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
+                  <Redirect to="/home" />
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+          
 
 
 
